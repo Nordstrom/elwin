ac442fd8ae39459123ffe163d745fb76a256312a
Maxwell <qij3@M00973369.nordstrom.net>
2015-12-17 17:02:59 -0800

    fixed stuff!

--- application.py
+++ application.py
@@ -1,42 +1,45 @@
 from flask import Flask
 import flask
-#from experiments.exp1 import ExampleExperiment
 from planout.experiment import SimpleInterpretedExperiment
 import json
+import os
 
-# pull in json experiment definition.
-class Exp1(SimpleInterpretedExperiment):
-    def loadScript(self):
-      self.script = json.loads(open("experiments/fredtest.json").read())
 
-class Exp2(SimpleInterpretedExperiment):
-    def loadScript(self):
-      self.script = json.loads(open("experiments/compliments.json").read())
+# function that generates experiment objects
 
-# return a dictionary with the experiment name and parameter for a userid
-def get_response(userid = '2'):
-    exp = Exp1(userid=userid)
-    exp2 = Exp2(userid=userid)
-    username = exp.get('name')
-    compliment = exp2.get('compliment')
-    outDict = {exp.name: username, exp2.name: compliment}
+def genExperiment(name, script, **inputs):
+    e = SimpleInterpretedExperiment(**inputs) # this allows us to create an expermeint object from a json definition
+    e.name = name # give it a name, this is used in hashing so these need to be unique
+    e.script = script # will pull in the json definition here
+    e.set_auto_exposure_logging(False)
+    return e
+
+#read in experiment definitions
+
+nameScriptDict = {}
+
+for exp in os.listdir("experiments"): #read in the experiment definitions, maybe store these in dynamo or something
+    name = exp.split(".")[0]
+    script = json.loads(open("experiments/" + exp).read())
+    nameScriptDict[name] = script
+
+# return experiment with params
+
+def get_response(experiment = 'atb_test', userid = 2):
+    outDict = {}
+    x = genExperiment(name = experiment, script = nameScriptDict[experiment], userid = userid).get_params()
+    outDict[experiment] = x
     return outDict
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda:
-    flask.jsonify(get_response())))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<userid>', 'foo', (lambda userid:
-    flask.jsonify(get_response(userid))))
+# add a rule when the page is accessed with unit appended
+application.add_url_rule('/<expname>/<userid>', 'exp', (lambda expname, userid:
+    flask.jsonify(get_response(expname, userid))))
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
     application.debug = True
     application.run()
